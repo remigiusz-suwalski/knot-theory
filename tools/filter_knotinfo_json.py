11e8fb082d79b07e87a638c1f93a976f6d8a8d9f
Remigiusz Suwalski <129324435+remigiusz-suwalski@users.noreply.github.com>
2022-02-28 18:47:39 +0100

    Describe how Jones collisions were found

--- tools/filter_knotinfo_json.py
+++ tools/filter_knotinfo_json.py
@@ -3,6 +3,31 @@
 import json
 import pandas as pd
 
+def jones_collisions(knots):
+    jones = dict()
+    for knot in knots.itertuples():
+        if knot.category not in [3, 4, 5, 6, 7, 8, 9, 10]:
+            break
+
+        key_1 = eval(knot.jones_polynomial_vector)
+        key_2 = [-key_1[1], -key_1[0]] + key_1[-1:1:-1]
+        key = min(str(key_1), str(key_2))
+        # 5_1    [2, 7, 1, 0, 1, -1, 1, -1]
+        # 10_132 [-7, -2, -1, 1, -1, 1, 0, 1]
+        # due to orientation issues
+
+        value = knot.name
+        if key not in jones:
+            jones[key] = list()
+        jones[key].append(value)
+
+    print("Jones does not distinguish:")
+    for v in jones.values():
+        if len(v) > 1:
+            print("/".join(v))
+    return
+
+
 with open("knotinfo_parsed.json") as f:
     query = '2 + crossing_number == 2 * braid_index'
     all_knots = pd.DataFrame(json.load(f)) 
@@ -10,4 +35,7 @@ with open("knotinfo_parsed.json") as f:
     
     print(f"{len(filtered_knots)} results!")
     for x in filtered_knots:
+        continue
         print(x)
+
+    # jones_collisions(all_knots)
