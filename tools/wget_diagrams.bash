b1854bf29b4a53e9f5a0e1b8bd631dcabfeb408e
Remigiusz Suwalski <129324435+remigiusz-suwalski@users.noreply.github.com>
2022-03-07 21:00:44 +0100

    Update knot diagrams downloader
    - switch to new URL
    - drop support for virtual knots (since they are not described in these
      notes anyway)

--- tools/wget_diagrams.bash
+++ tools/wget_diagrams.bash
@@ -5,17 +5,6 @@ virtuals=([0]="1" [2]="1" [3]="7" [4]="108")
 for crossing in "${!diagrams[@]}"; do
     for ((i=1;i<=${diagrams[$crossing]};i++)); do
         wget --wait=10 --random-wait \
-            "http://www.indiana.edu/~knotinfo/diagrams/${crossing}_$i.png"
+            "https://knotinfo.math.indiana.edu/diagrams/${crossing}_$i.png"
     done
 done
-
-for crossing in "${!virtuals[@]}"; do
-    for ((i=1;i<=${virtuals[$crossing]};i++)); do
-        wget \
-            --wait=10 \
-            --random-wait \
-            -O "virtual_${crossing}_$i.png" \
-            "http://www.math.toronto.edu/drorbn/Students/GreenJ/images/${crossing}.$i.png"
-    done
-done
-
